71282484c86e5e827f77b719a61b55a4946b8566
spr887011 <spr887011@gmail.com>
2023-05-25 22:11:56 +0530

    DBConnection.js added

---
+++ routes/DBConnection.js
@@ -0,0 +1,41 @@
+var express = require("express");
+var router = express.Router();
+var { MongoClient, ObjectId } = require("mongodb");
+const dotenv = require("dotenv");
+dotenv.config();
+var client = new MongoClient(
+  `mongodb+srv://${process.env.MongoDb}@cluster0.dfyktwl.mongodb.net/?retryWrites=true&w=majority`
+);
+
+async function CheckUser(emailIdLogin) {
+  var connection = await client.connect();
+  var db = connection.db(process.env.DB_name);
+  var loginCredentials = await db
+    .collection(process.env.UserRegistration_table)
+    .findOne({ email: emailIdLogin });
+  await connection.close();
+  return loginCredentials;
+}
+
+async function HomePage() {
+  var connection = await client.connect();
+  var db = connection.db(process.env.DB_name);
+  var restaurantList = await db
+    .collection(process.env.FoodAppReact_table)
+    .find({})
+    .toArray();
+  await connection.close();
+  return restaurantList;
+}
+
+async function DetailsPage(id) {
+  var connection = await client.connect();
+  var db = connection.db(process.env.DB_name);
+  var loginCredentials = await db
+    .collection(process.env.FoodAppReact_table)
+    .findOne({ _id:id });
+  await connection.close();
+  return loginCredentials;
+}
+
+module.exports = {CheckUser, HomePage,DetailsPage};
