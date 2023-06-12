fe78bb7eedab74f29b96992fbe89150542608b43
spr887011 <spr887011@gmail.com>
2023-06-12 13:58:14 +0530

    DBConnection.js file removed

--- routes/DBConnection.js
+++
@@ -1,6 +0,0 @@
-var { MongoClient, ObjectId } = require("mongodb");  
-const dotenv = require("dotenv");
-dotenv.config();
-var client = new MongoClient(
-  `mongodb+srv://${process.env.MongoDb}@cluster0.dfyktwl.mongodb.net/?retryWrites=true&w=majority`
-);
