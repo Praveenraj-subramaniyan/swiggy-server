6b183aebd17e5bbbbce45711ccdfefe98d6ca294
spr887011 <spr887011@gmail.com>
2023-06-15 22:02:13 +0530

    forget password hashed and stored

--- Controller/forgetpasswordController.js
+++ Controller/forgetpasswordController.js
@@ -1,5 +1,6 @@
 const UserRegistration = require("../Models/UserRegistration");
 const { sendMail } = require("../Controller/SendMail");
+const bcrypt = require("bcryptjs");
 
 async function ForgetPassword(email) {
   try {
@@ -18,7 +19,7 @@ async function ForgetPassword(email) {
         <p><b>Regards</b>,</p>
         <P>Swiggy Clone</p>
       `;
-       sendMail(email, "Forget Password - OTP", content);
+      sendMail(email, "Forget Password - OTP", content);
     }
     return true;
   } catch (error) {
@@ -30,32 +31,32 @@ function generateOTP() {
   const otp = Math.floor(100000 + Math.random() * 900000);
   return otp.toString();
 }
-async function NewPassword(email,otp,newPassword) {
+async function NewPassword(email, otp, newPassword) {
   try {
     const user = await UserRegistration.findOne({ email });
     if (user) {
       if (user.forgetPassword.otp === otp) {
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(newPassword, salt);
         user.forgetPassword = {};
-        user.password = newPassword;
+        user.password = hashedPassword;
         await user.save();
-      const content = `
+        const content = `
         <h4>Hi there,</h4>
         <p>Password chnaged successfully</p>
         <p><b>Regards</b>,</p>
         <P>Swiggy Clone</p>
       `;
-       sendMail(email, "New Password", content);
-       return true;
+        sendMail(email, "New Password", content);
+        return true;
+      } else {
+        return "Invalid OTP";
+      }
+    } else {
+      return "Invalid User";
     }
-    else{
-      return "Invalid OTP";
-    }
-  }
-  else{
-    return ("Invalid User");
-  }
   } catch (error) {
-    return  ("Invalid User");
+    return "Invalid User";
   }
 }
-module.exports = { ForgetPassword , NewPassword};
+module.exports = { ForgetPassword, NewPassword };
