6794b3cceedd5fc4821f9bedc462d9bddf187342
spr887011 <spr887011@gmail.com>
2023-09-03 17:39:53 +0530

    payment gateway changes finished

--- Controller/ordersController.js
+++ Controller/ordersController.js
@@ -1,7 +1,12 @@
 const UserRegistration = require("../Models/UserRegistration");
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+const stripe = require("stripe")(process.env.Stripe_Key);
+const { sendMail } = require("../Controller/SendMail");
 
-async function Addorders(email, orderList) {
+async function Addorders(email,token, orderList) {
+  let orderTotalPrice = 0; 
   try {
     const currentDate = new Date();
     const options = {
@@ -20,6 +25,8 @@ async function Addorders(email, orderList) {
       .replace(/:/g, ".");
     let orderListDetails = [];
     orderList.forEach((data) => {
+      const totalPriceForItem = data.quantity * data.price; // Calculate total price for each item
+      orderTotalPrice += totalPriceForItem; 
       orderListDetails.push({
         res_id: data.res_id.toString(),
         dish_id: data.dish_id,
@@ -38,6 +45,50 @@ async function Addorders(email, orderList) {
       { email: email },
       { $set: { cart: [] }, $push: { order: orders } }
     );
+    try {
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: orderTotalPrice,
+        currency: 'inr',
+        automatic_payment_methods: { enabled: true },
+      });
+    
+      const emailContent = `
+        <html>
+          <head>
+            <title>Payment Success</title>
+          </head>
+          <body>
+            <h1>Your Payment Was Successful!</h1>
+            <p>Payment Amount: ₹${orderTotalPrice}</p>
+            <p>Date and Time: ${currentTime}</p>
+            <p>Thank you for your payment.</p>
+            <p>Regards</p>
+            <p>Swiggy Clone</p>
+          </body>
+        </html>
+      `;
+    
+      sendMail(email, "Payment success", emailContent);
+    } catch (error) {
+      const emailContent = `
+        <html>
+          <head>
+            <title>Payment Failed</title>
+          </head>
+          <body>
+            <h1>Payment Failed</h1>
+            <p>Payment Amount: ₹${orderTotalPrice}</p>
+            <p>Date and Time: ${currentTime}</p>
+            <p>We regret to inform you that your payment has failed.</p>
+            <p>Please check your payment details and try again.</p>
+            <p>Regards</p>
+            <p>Swiggy Clone</p>
+          </body>
+        </html>
+      `;
+      sendMail(email, "Payment failed", emailContent);
+    }
+   
   } catch (error) {
     console.error(error);
   }
--- routes/orders.js
+++ routes/orders.js
@@ -4,45 +4,33 @@ const { AuthorizeUser } = require("../Controller/loginController");
 const { HomePage } = require("../Controller/homeController");
 const { Addorders } = require("../Controller/ordersController");
 const { ViewCart } = require("../Controller/cartController");
-const dotenv = require("dotenv");
-dotenv.config();
-const stripe = require("stripe")(process.env.Stripe_Key);
+
 
 router.post("/checkout", async (req, res) => {
   try {
-    const auth_token = req.headers.authorization.split(' ')[1];
-    const {token} = await req.body;
-    console.log(token.email)
-    console.log(token)
+    const auth_token = req.headers.authorization.split(" ")[1];
+    const { token } = await req.body;
     var loginCredentials = await AuthorizeUser(auth_token);
     if (loginCredentials === false) {
       res.status(200).send("login");
     } else {
-      try{
+      try {
         var restaurantList = await HomePage();
-        await stripe.customers.create({
-          email: token.email
-        })
-        .then(customer => console.log(customer.id))
-        .catch(error => console.error(error));
-
-        await Addorders(loginCredentials.email, ViewCart(restaurantList, loginCredentials))
+        await Addorders(loginCredentials.email,token,ViewCart(restaurantList, loginCredentials));
         res.status(200).json(true);
-      }
-      catch(error){
+      } catch (error) {
         res.status(400).json(false);
       }
-     
     }
   } catch (error) {
     console.log(error);
     res.status(500).send("login");
   }
-}); 
+});
 
 router.get("/view", async (req, res) => {
   try {
-    const auth_token = req.headers.authorization.split(' ')[1];
+    const auth_token = req.headers.authorization.split(" ")[1];
     var loginCredentials = await AuthorizeUser(auth_token);
     if (loginCredentials === false) {
       res.status(400).send("login");
